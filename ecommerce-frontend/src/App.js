892875684b812f2aa6a61169bd400359ec90b4a9
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-10-13 19:54:18 +1100

    React start

---
+++ ecommerce-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>welcome to ecommerce site.</h1>
+    </>
+  );
+}
+
+export default App;
